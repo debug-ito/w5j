b1f347c7440d69a4eeabb8c9396dbd03d9b58dd8
Toshio Ito <debug.ito@gmail.com>
2017-07-25 18:37:18 +0900

    Add TinkerPop.Parse.

---
+++ w5j-backend/src/W5J/DB/TinkerPop/Parse.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- |
+-- Module: W5J.DB.TinkerPop.Parse
+-- Description: parsers for responses from Gremlin Server
+-- Maintainer: Toshio Ito <debug.ito@gmail.com>
+--
+-- 
+module W5J.DB.TinkerPop.Parse
+       ( PropertyValue(..),
+         PropertyOne(..),
+         PropertyMany(..),
+         VertexID,
+         VertexLabel,
+         parseVertex
+       ) where
+
+import Control.Applicative ((<$>), empty)
+import Control.Monad (mapM)
+import Data.Aeson (FromJSON(parseJSON), Object, (.:), Value(Object,Array))
+import Data.Aeson.Types (Parser)
+import Data.Foldable (toList)
+import qualified Data.HashMap.Strict (HashMap)
+import Data.Text (Text)
+
+
+-- | The atomic (vertex) property value
+newtype PropertyValue a = PropertyValue { unPropertyValue :: a }
+                        deriving (Show,Eq,Ord)
+
+instance FromJSON a => FromJSON (PropertyValue a) where
+  parseJSON (Object obj) = PropertyValue <$> (obj .: "value")
+  parseJSON _ = empty
+
+-- | Property value of ONE cardinality
+newtype PropertyOne a = PropertyOne { unPropertyOne :: PropertyValue a }
+                      deriving (Show,Eq,Ord)
+
+instance FromJSON a => FromJSON (PropertyOne a) where
+  parseJSON v = do
+    values <- fmap unPropertyMany $ parseJSON v
+    case values of
+     [head_v] -> return $ PropertyOne head_v
+     _ -> empty
+
+-- | Property value of LIST/SET cardinality
+newtype PropertyMany a = PropertyMany { unPropertyMany :: [PropertyValue a] }
+                       deriving (Show,Eq,Ord)
+
+instance FromJSON a => FromJSON (PropertyMany a) where
+  parseJSON (Array arr) = fmap (PropertyMany . map PropertyValue) $ mapM parseJSON $ toList arr
+  parseJSON _ = empty
+
+type VertexID = Integer
+type VertexLabel = Text
+
+parseVertex :: (VertexID -> VertexLabel -> Object -> Parser a)
+            -- ^ @id -> label -> properties -> Parser@
+            -> Parser a
+parseVertex = undefined
+
