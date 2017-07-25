01f1e57782de2bfcec02fd5422e354cb45d679cc
Toshio Ito <debug.ito@gmail.com>
2017-07-25 10:02:33 +0900

    Main: add preliminary test for addWhat.

--- w5j-backend/app/Main.hs
+++ w5j-backend/app/Main.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
        (main) where
 
-import W5J.GremlinTest (main)
+import W5J.Time (zeroTime)
+import W5J.What (What(..))
+import W5J.DB.TinkerPop (Connection, withConnection, addWhat)
+
+main :: IO ()
+main = withConnection "localhost" 8182 mainWithConn
+
+mainWithConn :: Connection -> IO ()
+mainWithConn conn = print =<< addWhat conn what
+  where
+    what = What { whatId = 0,
+                  whatTitle = "what title",
+                  whatTime = Nothing,
+                  whatBody = "what body",
+                  whatTags = ["foo", "bar", "buzz"],
+                  whatCreatedAt = zeroTime,
+                  whatUpdatedAt = zeroTime
+                }
+
+
