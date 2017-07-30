0793777211b07e8f4d4ce8dc12f314637a02ce09
Toshio Ito <debug.ito@gmail.com>
2017-07-30 16:53:39 +0900

    Fix Eq and Ord instances of Interval. pass the tests.

--- w5j-backend/src/W5J/Interval.hs
+++ w5j-backend/src/W5J/Interval.hs
@@ -13,12 +13,32 @@ module W5J.Interval
          mapInterval
        ) where
 
-import Numeric.Interval.NonEmpty
-  ( Interval,
-    (...),
-    inf,
-    sup
-  )
+import qualified Numeric.Interval.NonEmpty as NI
+
+newtype Interval a = Interval { unNI :: NI.Interval a }
+                     deriving (Show)
+
+-- | Structural 'Eq' instance. This is the behavior of as of
+-- intervals-0.8. Before 0.8, the 'Eq' instance was defined
+-- differently.
+instance Eq a => Eq (Interval a) where
+  a == b = (inf a == inf b) && (sup a == sup b)
+
+-- | Same as 'Eq' instance.
+instance Ord a => Ord (Interval a) where
+  compare a b = let inf_ret = compare (inf a) (inf b)
+                in if inf_ret == EQ
+                   then compare (sup a) (sup b)
+                   else inf_ret
+
+(...) :: Ord a => a -> a -> Interval a
+a ... b = Interval ((NI....) a b)
+
+inf :: Interval a -> a
+inf = NI.inf . unNI
+
+sup :: Interval a -> a 
+sup = NI.sup . unNI
 
 mapInterval :: Ord b => (a -> b) -> Interval a -> Interval b
 mapInterval f aint = binf ... bsup
