8f00c24c14a9ee767846cdf7ca4e80f69536c784
Toshio Ito <debug.ito@gmail.com>
2017-07-30 12:25:24 +0900

    Aeson: add AWhat.

--- w5j-backend/src/W5J/Aeson.hs
+++ w5j-backend/src/W5J/Aeson.hs
@@ -6,7 +6,11 @@
 --
 -- 
 module W5J.Aeson
-       ( -- * When
+       ( -- * What
+         AWhat,
+         toAWhat,
+         fromAWhat,
+         -- * When
          AWhen,
          toAWhen,
          fromAWhen,
@@ -26,9 +30,10 @@ import Data.Aeson
     genericToEncoding, genericParseJSON
   )
 import qualified Data.Aeson.Types as AesonType
+import Data.Text (Text)
 import GHC.Generics (Generic)
 
-import W5J.Interval (Interval, inf, sup, (...))
+import W5J.Interval (Interval, inf, sup, (...), mapInterval)
 import W5J.Time
   ( TimeInstant, toEpochMsec, fromEpochMsec,
     TimeZone, tzToString, tzFromString
@@ -37,7 +42,7 @@ import W5J.What (What(..), WhatID, Tag)
 import W5J.When (When(..))
 import W5J.Where (Where(..), WhereID)
 
-newtype AInterval a = AInterval (Interval a)
+newtype AInterval a = AInterval { unAInterval :: Interval a }
                     deriving (Eq,Show,Ord)
 
 instance ToJSON a => ToJSON (AInterval a) where
@@ -52,7 +57,7 @@ instance (FromJSON a, Ord a) => FromJSON (AInterval a) where
                          <*> (o .: "to")
   parseJSON _ = empty
 
-newtype ATimeInstant = ATimeInstant TimeInstant
+newtype ATimeInstant = ATimeInstant { unATimeInstant ::TimeInstant }
                      deriving (Eq,Ord,Show)
 
 instance ToJSON ATimeInstant where
@@ -90,6 +95,9 @@ instance ToJSON AWhen where
 instance FromJSON AWhen where
   parseJSON = genericParseJSON aesonOpt
 
+instance Ord AWhen where
+  compare a b = compare (fromAWhen a) (fromAWhen b)
+
 toAWhen :: When -> AWhen
 toAWhen (When a b c) = AWhen (ATimeInstant a) b (ATimeZone c)
 
@@ -98,6 +106,7 @@ fromAWhen (AWhen (ATimeInstant a) b (ATimeZone c)) = When a b c
 
 
 newtype AWhere = AWhere Where
+                 deriving (Show,Eq,Ord)
 
 instance ToJSON AWhere where
   toJSON (AWhere w) = object [ ("where_id", toJSON $ whereId w),
@@ -116,3 +125,36 @@ fromAWhere :: AWhere -> Where
 fromAWhere (AWhere w) = w
 
 
+data AWhat =
+  AWhat
+  { _what_id :: !WhatID,
+    _title :: !Text,
+    _when :: !(Maybe (AInterval AWhen)),
+    _wheres :: ![AWhere],
+    _body :: !Text,
+    _tags :: ![Tag],
+    _created_at :: !ATimeInstant,
+    _updated_at :: !ATimeInstant
+  }
+  deriving (Show,Eq,Generic)
+
+instance ToJSON AWhat where
+  toEncoding = genericToEncoding aesonOpt
+
+instance FromJSON AWhat where
+  parseJSON = genericParseJSON aesonOpt
+
+toAWhat :: What -> AWhat
+toAWhat w = AWhat (whatId w) (whatTitle w) v_when v_wheres (whatBody w)
+            (whatTags w) (ATimeInstant $ whatCreatedAt w) (ATimeInstant $ whatUpdatedAt w)
+  where
+    v_when = fmap AInterval $ fmap (mapInterval toAWhen) $ whatWhen w
+    v_wheres = map toAWhere $ whatWheres w
+
+fromAWhat :: AWhat -> What
+fromAWhat aw = What (_what_id aw) (_title aw) v_when v_wheres (_body aw)
+               (_tags aw) (unATimeInstant $ _created_at aw) (unATimeInstant $ _updated_at aw)
+  where
+    v_when = fmap (mapInterval fromAWhen) $ fmap unAInterval $ _when aw
+    v_wheres = map fromAWhere $ _wheres aw
+    
