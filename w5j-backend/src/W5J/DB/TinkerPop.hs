63973562fc854c99396bb55a9d9dba6e919f16a7
Toshio Ito <debug.ito@gmail.com>
2017-08-06 18:08:36 +0900

    TinkerPop: fix query Gremlin of getWhatById.

--- w5j-backend/src/W5J/DB/TinkerPop.hs
+++ w5j-backend/src/W5J/DB/TinkerPop.hs
@@ -97,7 +97,7 @@ getWhatById conn wid = do
    Just got_val -> fmap (Just . unACompleteWhat) $ ioFromJSON got_val
   where
     gremlin = "g.V(WID).hasLabel('what')"
-              <> ".map({ v = it.get(); [__(v).valueMap().toList(), __(v).out('when_from').toList(), __(v).out('when_to').toList(), "
+              <> ".map({ v = it.get(); [v, __(v).out('when_from').toList(), __(v).out('when_to').toList(), "
               <>  "__(v).out('where').toList()]})"
     binds = HM.fromList [("WID", toJSON wid)]
 
