f62588a0fd6af086dc9163e1f01c0711b1925f9f
Toshio Ito <debug.ito@gmail.com>
2017-08-13 16:40:31 +0900

    Query.What: partially implement buildQuery. still need work..

--- w5j-backend/src/W5J/DB/TinkerPop/Query/What.hs
+++ w5j-backend/src/W5J/DB/TinkerPop/Query/What.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 -- |
 -- Module: W5J.DB.TinkerPop.Query.What
 -- Description: queries for What
@@ -6,13 +7,16 @@
 -- 
 module W5J.DB.TinkerPop.Query.What
        ( QueryWhat,
+         buildQuery,
          QOrderBy(..),
          QCond(..)
        ) where
 
+import Data.Monoid ((<>))
 import Data.Text (Text)
 
-import W5J.DB.TinkerPop.Query.Common (Query)
+import W5J.DB.TinkerPop.GBuilder (GBuilder, newPlaceHolder, Gremlin)
+import W5J.DB.TinkerPop.Query.Common (Query, QOrder(..), buildQueryWith)
 
 type QueryWhat = Query QCond QOrderBy
 
@@ -24,3 +28,14 @@ data QOrderBy = QOrderByWhen
 data QCond = QCondTerm Text
              -- ^ free term search entry for title, body, tags.
              deriving (Show,Eq,Ord)
+
+buildQuery :: QueryWhat -> GBuilder Gremlin
+buildQuery = buildQueryWith buildCond buildOrder
+  where
+    buildCond (QCondTerm t) = do
+      vt <- newPlaceHolder t
+      return (".or(__.has('title', textContains(" <> vt <> "))"
+              <> ", __.has('body', textContains(" <> vt <> "))"
+              <> ", __.has('tags', eq(" <> vt <> ")))")
+    buildOrder order QOrderByWhen = undefined
+      
