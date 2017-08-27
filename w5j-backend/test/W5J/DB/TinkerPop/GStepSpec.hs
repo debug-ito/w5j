f4fb54cf92bb20a03b4d7689fb1a1dff7e2dbf35
Toshio Ito <debug.ito@gmail.com>
2017-08-27 18:38:34 +0900

    try writing GStepSpec with hint package.

---
+++ w5j-backend/test/W5J/DB/TinkerPop/GStepSpec.hs
@@ -0,0 +1,36 @@
+module W5J.DB.TinkerPop.GStepSpec (main,spec) where
+
+import Language.Haskell.Interpreter
+  ( loadModules, OptionVal((:=)), set, searchPath,
+    setTopLevelModules, runInterpreter, InterpreterError,
+    typeChecks
+  )
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "Logic typeclass" $ do
+  checkLogicCompatible "Filter" "Filter" True
+
+toErrString :: Either InterpreterError a -> Either String a
+toErrString (Right a) = Right a
+toErrString (Left e) = Left $ show e
+
+checkLogicCompatible :: String -> String -> Bool -> Spec
+checkLogicCompatible child parent expected = specify label $ doCheck
+  where
+    label = child ++ " -> " ++ parent
+    doCheck = do
+      result <- fmap toErrString $ runInterpreter isTypeOK
+      result `shouldBe` Right expected
+    isTypeOK = do
+      set [searchPath := ["src"]]
+      loadModules ["src/W5J/DB/TinkerPop/GStep.hs"]
+      setTopLevelModules ["W5J.DB.TinkerPop.GStep"]
+      typeChecks code
+    code =    "hoge :: GStep " ++ parent ++ " s s\n"
+           ++ "hoge = gFilter x\n"
+           ++ "  where x :: GStep " ++ child ++ " s s"
+           ++ "        x = undefined"
